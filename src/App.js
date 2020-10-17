d107b03df7e6de3d00d6805739dd622ddb4eb117
AlphaGamer5 <61914586+AlphaGamer5@users.noreply.github.com>
2020-10-17 11:56:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import AutoGrid from './components/masonry/masonry'
+import "./styles.css";
+
+
+export default function App() {
+  return (
+    <>
+      <AutoGrid/>
+    </>
+  )
+}
---
+++ src/components/masonry/masonry.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import About from '../about/about'
+import SignUp from '../register/register';
+import Rules from '../rules/rules';
+import Judgement from '../judgement/judgement';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function AutoGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={5}>
+          <Paper className={classes.paper}><About/></Paper>
+        </Grid>
+        <Grid item xs={12} lg={7}>
+          <Paper className={classes.paper}><Rules/></Paper>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={5}>
+          <Paper className={classes.paper}><Judgement/></Paper>
+        </Grid>
+        <Grid item xs={12} lg={7}>
+          <Paper className={classes.paper}><SignUp/></Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
